dffc0ea932da49b4075bb413e4b09557d21bd352
ranju <ranju1729@gmail.com>
2021-09-24 18:19:02 +0530

    Initial Commit

--- Library_Management_Backend/Book_Shelf_Operations/urls.py
+++ Library_Management_Backend/Book_Shelf_Operations/urls.py
@@ -4,5 +4,4 @@ from .views import BookShelfViewInsert, BookShelfUpdateDelete
 urlpatterns = [
     path('bookshelf/listadd/api/V1', BookShelfViewInsert.as_view({'get' : 'list', 'post' : 'create'})),
     path('bookshelf/updatedelete/api/v1/<int:id>', BookShelfUpdateDelete.as_view({'get' : 'retrieve', 'put' : 'update',
-                                                                         'delete' : 'destroy'}))
-]
+                                                                         'delete' : 'destroy'})) ]
